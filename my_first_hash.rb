a7413603bafec685ce3522038a3d1ab5e697ff42
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-17 19:48:02 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   hash={"index0"=>0,"index1"=>1,"index2"=>2,"index3"=>3}
-  hash
 end
 
 
